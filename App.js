9a56f821e2840a45c0283f335505d4d9e00ab38a
Raphael Onofre <raphael.onofre@gmail.com>
2019-04-20 10:21:15 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import logo from './assets/logo.png'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.findMyIp = this.findMyIp.bind(this)
+    this.state = {
+      data: ''
+    }
+  }
+
+  async findMyIp() {
+    this.setState({
+      data: 'Descobrindo meu ip...'
+    })
+    const ip = await fetch('http://httpbin.org/ip')
+    const data = await ip.json()
+    const meuIp = data.origin.split(",")[0]
+    this.setState({
+      data: meuIp
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Image source={logo} />
+          <Text style={styles.ip}>{this.state.data}</Text>
+          <Button title='Descobrir meu IP!' onPress={this.findMyIp}/>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.made}>Feito com ♥ por Raphael Onofre</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2F2336',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  ip: {
+    color: 'white',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  footer: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  made: {
+    color: 'white',
+    textAlign: 'center'
+  }
+});
